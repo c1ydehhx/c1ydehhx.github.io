e1aa03a5d6f2cb0ba973adc18256136e482d64b9
ntut-xuan <t109590031@ntut.org.tw>
2025-12-31 06:28:05 +0800

    Fix wrong description

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -18,7 +18,7 @@ type PostMeta = {
 const postMetaMap: Record<string, PostMeta> = {
   "2025f-free5gc-dpi": {
     title: "c1ydeh's page - Efficient and Hardware-Independent Deep Packet Inspection for Free5GC",
-    description: "This is the technical article to record the step of issue solving on solvcon/solvcon GitHub Actions. Due to lack of shared object of packaging, it's necessary to clearify the problem first, then trying to find a good practice to solve the issue.",
+    description: "This is the technical article for this project started in 2025 fall. We aims to introduce software-based deep packet inspection (DPI) that having the same efficient on hardware-support solution. We propose 3 different method: P4 Assist Method, Full Copy Method, and XDP Method.",
     coverImage: "/image/blog/2025f-free5gc-dpi/cover.png",
   },
   "2025f-deep-dive-setup-py": {
